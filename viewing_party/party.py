2cebff06164d144a1f41035c52befad23ab03f30
sashavershkova <a.m.vershkova@gmail.com>
2025-03-24 11:42:07 -0700

    defined created add_to_wathed function. passed 2 more tests

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -10,6 +10,10 @@ def create_movie(title, genre, rating):
         movie["rating"] = rating
     return movie
 
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
