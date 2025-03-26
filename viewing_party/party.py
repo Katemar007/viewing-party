cf077503798050ed841ab4051cdbd4cf5d5f9496
sashavershkova <a.m.vershkova@gmail.com>
2025-03-26 15:40:50 -0700

    add get_available_recs func, passed wave 4

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -101,17 +101,17 @@ def get_friends_unique_watched(user_data):
                 unique_watched.append(mov)
             
     return unique_watched
-
-
-
-
-
-
-
         
-# -----------------------------------------
 # ------------- WAVE 4 --------------------
-# -----------------------------------------
+
+def get_available_recs(user_data):
+
+    friends_watched = get_friends_unique_watched(user_data)
+    recommended_movies = []
+    for movie in friends_watched:
+        if movie["host"] in user_data["subscriptions"]:
+            recommended_movies.append(movie)
+    return recommended_movies
 
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
